6a32b4c0743a77506ddceb3aeca0f4a788ceb7d3
AchuthKrishna <92132574+AchuthKrishna@users.noreply.github.com>
2022-02-03 19:09:02 -0800

    Fixed the test

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTester {
     @Test
     public void testMethod(){
-        assertEquals(7, SkillDemo.multiply(5, 2));
+        assertEquals(10, SkillDemo.multiply(5, 2));
     }
 }

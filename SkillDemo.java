c09324364b1436ab4d59aed4c38261c941db21d6
AchuthKrishna <92132574+AchuthKrishna@users.noreply.github.com>
2022-02-03 19:05:32 -0800

    Added lib folder and Tester file

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,5 @@
-//paste code in later
+public class SkillDemo{
+    public static int multiply(int x, int y){
+        return x * y;
+    }
+}
---
+++ SkillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    @Test
+    public void testMethod(){
+        assertEquals(7, SkillDemo.multiply(5, 2));
+    }
+}
